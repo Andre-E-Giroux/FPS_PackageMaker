59d4cb0551f2ee1cd5f8ebf9adfc6efd80e0c7f7
DampArsonist <90652931+Andre-E-Giroux@users.noreply.github.com>
2022-12-04 19:03:38 -0500

    initial commit

--- Assets/FirstPersonPlayerController/Scripts/GameManager.cs
+++ Assets/FirstPersonPlayerController/Scripts/GameManager.cs
@@ -1,18 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public void GoToBuildScene()
     {
-        
+        SceneManager.LoadScene("BuildScene");
     }
 
-    // Update is called once per frame
-    void Update()
+    public void GoToTitleScene()
     {
-        
+        SceneManager.LoadScene("TitleScene");
     }
 }
